87d56e887ac08da9a15ee89e3312c83f3cce46d3
jaltrock <altrockweb@gmail.com>
2024-11-25 15:05:01 -0500

    Added deleteProduct function

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -1,37 +1,35 @@
 import { useState } from "react"
 
-let products = [
+let initialProducts = [
     { name: "Samsung Galaxy 100", brand: "Samsung", category: "Phones", unitPrice:899, quantity:3 },
     { name: "iPhone14", brand: "Apple", category: "phones", unitPrice:949, quantity:2 },
     { name: "HP Envy 100", brand: "HP", category: "Computers", unitPrice:1199, quantity:1 }
 ]
 
 export default function ProductList() {
-    /*
-    let productElements = []
-    for (let i = 0; i < products.length; i++) {
-        productElements.push(<ProductItem key={i} name={products[i].name}
-        brand={products[i].brand}
-        category={products[i].category}
-        unitPrice={products[i].unitPrice}
-        quantity={products[i].quantity} />)
-    }
-*/
+   let [products, setProducts] = useState(initialProducts)
 
+   function deleteProduct(index) {
+        let newProducts = [...products]
+        newProducts.splice(index, 1)
+        setProducts(newProducts)
+   }
 
-return (
-    <div className="container py-5">
-        {
-            products.map((product, idx) => {
-                return (
-                    <ProductItem key={idx} name={product.name}
-                        brand={product.brand}
-                        category={product.category}
-                        unitPrice={product.unitPrice}
-                        quantity={product.quantity} />
-                )
-            })
-        }
+    return (
+        <div className="container py-5">
+            {
+                products.map((product, idx) => {
+                    return (
+                        <ProductItem key={idx} name={product.name}
+                            brand={product.brand}
+                            category={product.category}
+                            unitPrice={product.unitPrice}
+                            quantity={product.quantity} 
+                            index={idx}
+                            deleteProduct={deleteProduct}/>
+                    )
+                })
+            }
         </div>
     )
 }
@@ -65,7 +63,9 @@ function ProductItem(props) {
                     <span>${props.unitPrice * quantity}</span>
                 </div>
                 <div className="col-4">
-                    <button type="button" className="btn btn-danger btn-sm">Delete</button>
+                    <button type="button" 
+                    className="btn btn-danger btn-sm"
+                    onClick={() => props.deleteProduct(props.index)}>Delete</button>
                 </div>
         </div>
     )
